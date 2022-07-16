5ec9e46448632eb86022fb75b84130e764d5d903
Matias <matibalbi@gmail.com>
2022-07-15 21:05:53 -0300

    primeros cambios responsive

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -4,10 +4,11 @@ import './Landing.css';
 const Landing = () => {
     return (
         <div className="backgroundLanding">
-            <div className='wordsLuigi'>HEY!</div>
+            {/* <div className='wordsLuigi'>HEY!</div>
             <div className='wordsYoshi'>WELCOME TO...</div>
-            <div className='wordsMario'>...VIDEO GAMES APP!</div>
-            <div className='containerButtonIdNotFound'>
+            <div className='wordsMario'>...VIDEO GAMES APP!</div> */}
+            <div className='containerLanding'>    
+                <div className='landingTitle'>WELCOME TO VIDEO GAMES APP!</div>
                 <Link to="/home">
                     <button className="landingButton">Enter</button>
                 </Link>
