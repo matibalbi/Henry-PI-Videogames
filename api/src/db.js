b741b80531662fbbb781de160c49f0b06ef082e1
Matias Balbi <matibalbi@gmail.com>
2023-01-14 13:28:20 -0300

    Deploy back

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 
 const sequelize = 
   process.env.NODE_ENV === "production"
@@ -27,7 +27,11 @@ const sequelize =
         },
         ssl: true,
       })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+    // : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+    //     logging: false, // set to console.log to see the raw SQL queries
+    //     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    //   });
+    : new Sequelize(DB_DEPLOY, {
         logging: false, // set to console.log to see the raw SQL queries
         native: false, // lets Sequelize know we can use pg-native for ~30% more speed
       });
